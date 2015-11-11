d2983e2dd16df87da1729ceae24b88eba946120a
Todd Ruhlen <rhythmtome@yahoo.com>
2015-11-10 22:03:01 -0500

    Complete chapter 6

--- chapter6.js
+++ chapter6.js
@@ -26,3 +26,59 @@ Vector.prototype.plus = function(vect) {
 Vector.prototype.minus = function(vect) {
   return new Vector(this.x - vect.x, this.y - vect.y);
 };
+
+// StretchCell wrapper
+
+function StretchCell(inner, minW, minH) {
+  this.inner = inner;
+  this.minW = minW;
+  this.minH = minH;
+}
+StretchCell.prototype.minWidth = function() {
+  if (this.inner.minWidth() <= this.minW)
+    return this.minW;
+  else
+    return this.inner.minWidth();
+};
+StretchCell.prototype.minHeight = function() {
+    if (this.inner.minHeight() <= this.minH)
+    return this.minH;
+  else
+    return this.inner.minHeight();
+};
+StretchCell.prototype.draw = function(width, height) {
+  return this.inner.draw(width,height);
+};
+
+// A more concise version of the minWidth/Height functions of the above
+
+StretchCell.prototype.minWidth = function() {
+  return Math.max(this.minW, this.inner.minWidth())
+};
+StretchCell.prototype.minHeight = function() {
+  return Math.max(this.minH, this.inner.minHeight())
+};
+
+// Sequence interface, implemented for arrays and ranges(ranges construced by the sequence object)
+
+function ArraySeq(array) {
+  this.seqObj = array;
+  this.length = array.length;
+}
+function RangeSeq(from, to) {
+  this.makeRange = function(from, to) {
+    var range = [];
+    for (var i = from; i < to - from; i++) {
+      range.push(i);
+    }
+    return range;
+  };
+  this.seqObj = this.makeRange(from, to);
+  this.length = this.seqObj.length;
+}
+function logFive(sequence) {
+  var length = Math.min(5, sequence.length);
+  for (var i = 0; i < length; i++) {
+    console.log(sequence.seqObj[i]);
+  }
+}
